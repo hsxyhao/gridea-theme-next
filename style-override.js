61d2b80a329e6e1554d942235323aecbf84b275e
hsxyhao <806649821@qq.com>
2020-03-22 03:57:42 +0800

    添加自定义友链并优化透明效果

--- style-override.js
+++ style-override.js
@@ -1,14 +1,44 @@
+function toHexColor(color, opacity) {
+  opacity = opacity || 255;
+  if (color.length === 4) {
+    return `#${color[1]}${color[1]}${color[2]}${color[2]}${color[3]}${color[3]}${opacity.toString(16)}`;
+  } else if (color.length === 7) {
+    return `#${color}${opacity.toString(16)}`;
+  }
+  return color;
+}
+
 const generateOverride = (params) => {
   let result = '';
   let headerClass = 'header'
   let cdn = params.cdn || '';
+  let opacity = 100;
+  if (params.opacity) {
+    try {
+      opacity = parseFloat(params.opacity)
+      if (opacity < 0 || opacity > 100) {
+        opacity = 85;
+        console.warn("Opacity ill，use default value of 0.85");
+      }
+    } catch(err) {
+      console.warn("Opacity parse float error，use default value of 0.85")
+    }
+  }
+
+  if (opacity < 100) {
+    result += `.bg-color .bg-color {
+      background: transparent !important;
+    }`
+  }
+  let ratio = parseInt(255 / 100.0 * opacity);
+
   // 默认配置
   if (!params.bgColor) {
     let bgColor = '';
     if (params.scheme === 'mist') {
         headerClass = 'blog-header'
         result += `.mist .header {
-          background-color: #F5F5F3;
+          background-color: #F5F5F3${ratio.toString(16)};
         }`
         bgColor = '#fff';
     } else if (params.scheme === 'muse') {
@@ -20,11 +50,12 @@ const generateOverride = (params) => {
     } else {
       bgColor = '#fff';
     }
+    bgColor = toHexColor(bgColor, ratio);
     result += `body {
       background-color: ${bgColor} !important;
     }
     .nav-toggle .line {
-      background-color: ${params.bgColor} !important;
+      background-color: ${bgColor} !important;
     }`
   } else {
     result += `body {
@@ -75,9 +106,10 @@ const generateOverride = (params) => {
   }
 
   if (params.timeLineColor) {
+    let color = toHexColor(params.timeLineColor, ratio);
     result += `
     .archive-timeline-box::after, .tag-timeline-wrapper::after {
-      background: ${params.timeLineColor};
+      background: ${color};
     }`
   }
 
@@ -96,16 +128,6 @@ const generateOverride = (params) => {
     
     .node-title::before {
       background-color: ${params.titleColor};
-    }`
-  }
-
-  if (params.postSectionBgColor) {
-    result += `.bg-color {
-      background-color: ${params.postSectionBgColor};
-    }`
-  } else {
-    result += `.bg-color {
-      background-color: transparent;
     }`
   }
 
@@ -171,12 +193,13 @@ const generateOverride = (params) => {
   }
 
   if (params.menuActiveColor) {
+    let color = toHexColor(params.menuActiveColor, ratio);
     result += `.nav-item-active {
-      background: ${params.menuActiveColor};
+      background: ${color};
     }
 
     .nav-item:hover {
-      background: ${params.menuActiveColor};
+      background: ${color};
       color: rgba(0, 0, 0, 0.98);
     }`
   }
@@ -299,85 +322,25 @@ const generateOverride = (params) => {
   }
 
   // 魔改
-
-  if (params.opacity) {
-    let opacity = 0.85;
-    try {
-      opacity = parseFloat(params.opacity) / 100.0
-      if (opacity < 0 || opacity > 1) {
-        opacity = 0.85;
-        console.warn("Opacity ill，use default value of 0.85");
-      }
-    } catch(err) {
-      console.warn("Opacity parse float error，use default value of 0.85")
-    }
-    if (params.scheme === 'pisces' || params.scheme === 'gemini') {
-      // 首页 阅读页 归档 标签
-      result += `.site-nav, .sidebar-wrapper, .section-box, .sidebar-box, .site-nav, .blog-header {
-        opacity: ${opacity};
-      }`
-    } else if (params.scheme === 'muse'){
-      // 首页 阅读页 归档 标签
-      result += `.header {
-        opacity: ${opacity};
-        background-color: #fff;
-      }
-      .muse.section-layout {
-        opacity: ${opacity};
-      }`
-    } else {
-      // 首页 阅读页 归档 标签
-      result += `.section-layout {
-        opacity: ${opacity} !important;
-      }`
-    }
-  }
-  if (params.headerImg) {
-    if (params.scheme === 'muse') {
-      result += `
-        .header-box {
-          background-image: url(${cdn}${params.headerImg});
-          background-attachment: fixed;
-        }
-        .muse .blog-header {
-          padding: 50px 0 0 !important;
-          background-color: transparent !important;
-          overflow: unset !important;
-        }
-        .site-nav ul {
-          background: rgba(255,255,255,0.65);
-          box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.15);
-          padding: 10px 100px !important;
-        }
-        .site-nav {
-          margin: 40px 0 0 !important;
-        }
-        .muse.section-layout {
-          padding: 0px !important;
-          background-color: transparent !important;
-        }
-        .muse .post {
-          padding: 20px 40px !important;
-          -webkit-box-shadow: 0 0 50px rgba(202,203,203,0.5);
-          -moz-box-shadow: 0 0 5px rgba(202,203,204,0.5);
-        }
-      `;
-    } else if (params.scheme === 'pisces' || params.scheme === 'gemini') {
-      result += `.site-meta {
-        background: transparent !important;
-        background-image: url(${params.headerImg}) !important;
-      }`;
-    }
-    
+  if (params.postSectionBgColor) {
+    let color = toHexColor(params.postSectionBgColor, ratio);
+    result += `.bg-color {
+      background-color: ${color};
+    }`
+  } else {
+    result += `.bg-color {
+      background-color: #ffffff${ratio.toString(16)};
+    }`
   }
 
   if (params.bgSiteMetaColor) {
+    let color = params.bgSiteMetaColor;
     result += `
       .site-meta {
-        background: ${params.bgSiteMetaColor} !important;
+        background: ${color} !important;
       }
       .head-top-line {
-        background: ${params.bgSiteMetaColor} !important;
+        background: ${color} !important;
       }`;
   }
 
@@ -407,9 +370,10 @@ const generateOverride = (params) => {
   }
 
   if (params.bgTitleColor) {
+    let color = toHexColor(params.bgTitleColor, ratio);
     result += `
     .brand {
-      background: ${params.bgTitleColor};
+      background: ${color};
     }
     `
   } else {
@@ -435,11 +399,12 @@ const generateOverride = (params) => {
   }
 
   if (params.blogTitleColor) {
+    let color = toHexColor(params.blogTitleColor, ratio);
     result += `.main-title {
       color: ${params.blogTitleColor};
     }
     .mist .site-title i {
-      background: ${params.blogTitleColor};
+      background: ${color};
     }`
   }
 
