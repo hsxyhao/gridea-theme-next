3d4c598472108b9dc4e451fdfe82de42bc9336bc
hsxyhao <806649821@qq.com>
2020-06-03 14:08:43 +0800

    优化粒子时钟

--- assets/media/js/magic/clock.js
+++ assets/media/js/magic/clock.js
@@ -1,257 +1,274 @@
-(function () {
+(function(){
+  var WINDOW_WIDTH = 820;
+  var WINDOW_HEIGHT = 250;
+  var RADIUS = 7; 
+  var NUMBER_GAP = 10;
+  var u=0.65; 
+  var context;
+  var balls = [];
+  const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"]; //褰╄壊灏忕悆鐨勯鑹�
+  var currentNums = []; 
   var digit =
-    [
       [
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 1, 1, 0, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 0, 1, 1, 0],
-        [0, 0, 1, 1, 1, 0, 0]
-      ],//0
-      [
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 1, 1, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1]
-      ],//1
-      [
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1]
-      ],//2
-      [
-        [1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0]
-      ],//3
-      [
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 1, 0],
-        [0, 0, 1, 1, 1, 1, 0],
-        [0, 1, 1, 0, 1, 1, 0],
-        [1, 1, 0, 0, 1, 1, 0],
-        [1, 1, 1, 1, 1, 1, 1],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 1, 1]
-      ],//4
-      [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0]
-      ],//5
-      [
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 1, 1, 0, 0, 0, 0],
-        [1, 1, 0, 0, 0, 0, 0],
-        [1, 1, 0, 1, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0]
-      ],//6
-      [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 1, 1, 0, 0, 0],
-        [0, 0, 1, 1, 0, 0, 0]
-      ],//7
-      [
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 1, 1, 0]
-      ],//8
-      [
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 0, 0, 1, 1],
-        [0, 1, 1, 1, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 0, 1, 1],
-        [0, 0, 0, 0, 1, 1, 0],
-        [0, 0, 0, 1, 1, 0, 0],
-        [0, 1, 1, 0, 0, 0, 0]
-      ],//9
-      [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]
-      ]//:
-    ];
+          [
+              [0,0,1,1,1,0,0],
+              [0,1,1,0,1,1,0],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,0,1,1,0],
+              [0,0,1,1,1,0,0]
+          ],//0
+          [
+              [0,0,0,1,1,0,0],
+              [0,1,1,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [1,1,1,1,1,1,1]
+          ],//1
+          [
+              [0,1,1,1,1,1,0],
+              [1,1,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,0,0],
+              [0,0,1,1,0,0,0],
+              [0,1,1,0,0,0,0],
+              [1,1,0,0,0,0,0],
+              [1,1,0,0,0,1,1],
+              [1,1,1,1,1,1,1]
+          ],//2
+          [
+              [1,1,1,1,1,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,0,0],
+              [0,0,1,1,1,0,0],
+              [0,0,0,0,1,1,0],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,1,1,0]
+          ],//3
+          [
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,1,0],
+              [0,0,1,1,1,1,0],
+              [0,1,1,0,1,1,0],
+              [1,1,0,0,1,1,0],
+              [1,1,1,1,1,1,1],
+              [0,0,0,0,1,1,0],
+              [0,0,0,0,1,1,0],
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,1,1]
+          ],//4
+          [
+              [1,1,1,1,1,1,1],
+              [1,1,0,0,0,0,0],
+              [1,1,0,0,0,0,0],
+              [1,1,1,1,1,1,0],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,1,1,0]
+          ],//5
+          [
+              [0,0,0,0,1,1,0],
+              [0,0,1,1,0,0,0],
+              [0,1,1,0,0,0,0],
+              [1,1,0,0,0,0,0],
+              [1,1,0,1,1,1,0],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,1,1,0]
+          ],//6
+          [
+              [1,1,1,1,1,1,1],
+              [1,1,0,0,0,1,1],
+              [0,0,0,0,1,1,0],
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,0,0],
+              [0,0,0,1,1,0,0],
+              [0,0,1,1,0,0,0],
+              [0,0,1,1,0,0,0],
+              [0,0,1,1,0,0,0],
+              [0,0,1,1,0,0,0]
+          ],//7
+          [
+              [0,1,1,1,1,1,0],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,1,1,0],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,1,1,0]
+          ],//8
+          [
+              [0,1,1,1,1,1,0],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [1,1,0,0,0,1,1],
+              [0,1,1,1,0,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,0,1,1],
+              [0,0,0,0,1,1,0],
+              [0,0,0,1,1,0,0],
+              [0,1,1,0,0,0,0]
+          ],//9
+          [
+              [0,0,0,0],
+              [0,0,0,0],
+              [0,1,1,0],
+              [0,1,1,0],
+              [0,0,0,0],
+              [0,0,0,0],
+              [0,1,1,0],
+              [0,1,1,0],
+              [0,0,0,0],
+              [0,0,0,0]
+          ]//:
+      ];
 
-  var canvas = document.getElementById('canvasDiyBlock');
+  function drawDatetime(cxt){
+      var nums = [];
 
-  if (canvas.getContext) {
-    var cxt = canvas.getContext('2d');
-    //声明canvas的宽高
-    var H = 100, W = 700;
-    canvas.height = H;
-    canvas.width = W;
-    cxt.fillStyle = '#f00';
-    cxt.fillRect(10, 10, 50, 50);
+      context.fillStyle="#005eac"
+      var date = new Date();
+      var offsetX = 70, offsetY = 30;
+      var hours = date.getHours();
+      var num1 = Math.floor(hours/10);
+      var num2 = hours%10;
+      nums.push({num: num1});
+      nums.push({num: num2});
+      nums.push({num: 10}); 
+      var minutes = date.getMinutes();
+      var num1 = Math.floor(minutes/10);
+      var num2 = minutes%10;
+      nums.push({num: num1});
+      nums.push({num: num2});
+      nums.push({num: 10}); 
+      var seconds = date.getSeconds();
+      var num1 = Math.floor(seconds/10);
+      var num2 = seconds%10;
+      nums.push({num: num1});
+      nums.push({num: num2});
 
-    //存储时间数据
-    var data = [];
-    //存储运动的小球
-    var balls = [];
-    //设置粒子半径
-    var R = canvas.height / 20 - 1;
-    (function () {
-      var temp = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date());
-      //存储时间数字，由十位小时、个位小时、冒号、十位分钟、个位分钟、冒号、十位秒钟、个位秒钟这7个数字组成
-      data.push(temp[1], temp[2], 10, temp[3], temp[4], 10, temp[5], temp[6]);
-    })();
-
-    /*生成点阵数字*/
-    function renderDigit(index, num) {
-      for (var i = 0; i < digit[num].length; i++) {
-        for (var j = 0; j < digit[num][i].length; j++) {
-          if (digit[num][i][j] == 1) {
-            cxt.beginPath();
-            cxt.arc(14 * (R + 2) * index + j * 2 * (R + 1) + (R + 1), i * 2 * (R + 1) + (R + 1), R, 0, 2 * Math.PI);
-            cxt.closePath();
-            cxt.fill();
+      for(var x = 0;x<nums.length;x++){
+          nums[x].offsetX = offsetX;
+          offsetX = drawSingleNumber(offsetX,offsetY, nums[x].num,cxt);
+          if(x<nums.length-1){
+              if((nums[x].num!=10) &&(nums[x+1].num!=10)){
+                  offsetX+=NUMBER_GAP;
+              }
           }
-        }
       }
-    }
 
-    /*更新时钟*/
-    function updateDigitTime() {
-      var changeNumArray = [];
-      var temp = /(\d)(\d):(\d)(\d):(\d)(\d)/.exec(new Date());
-      var NewData = [];
-      NewData.push(temp[1], temp[2], 10, temp[3], temp[4], 10, temp[5], temp[6]);
-      for (var i = data.length - 1; i >= 0; i--) {
-        //时间发生变化
-        if (NewData[i] !== data[i]) {
-          //将变化的数字值和在data数组中的索引存储在changeNumArray数组中
-          changeNumArray.push(i + '_' + (Number(data[i]) + 1) % 10);
-        }
-      }
-      //增加小球
-      for (var i = 0; i < changeNumArray.length; i++) {
-        addBalls.apply(this, changeNumArray[i].split('_'));
-      }
-      data = NewData.concat();
-    }
-
-    /*更新小球状态*/
-    function updateBalls() {
-      for (var i = 0; i < balls.length; i++) {
-        balls[i].stepY += balls[i].disY;
-        balls[i].x += balls[i].stepX;
-        balls[i].y += balls[i].stepY;
-        if (balls[i].x > W + R || balls[i].y > H + R) {
-          balls.splice(i, 1);
-          i--;
-        }
-      }
-    }
-
-    /*增加要运动的小球*/
-    function addBalls(index, num) {
-      var numArray = [1, 2, 3];
-      var colorArray = ["#3BE", "#09C", "#A6C", "#93C", "#9C0", "#690", "#FB3", "#F80", "#F44", "#C00"];
-      for (var i = 0; i < digit[num].length; i++) {
-        for (var j = 0; j < digit[num][i].length; j++) {
-          if (digit[num][i][j] == 1) {
-            var ball = {
-              x: 14 * (R + 2) * index + j * 2 * (R + 1) + (R + 1),
-              y: i * 2 * (R + 1) + (R + 1),
-              stepX: Math.floor(Math.random() * 4 - 2),
-              stepY: -2 * numArray[Math.floor(Math.random() * numArray.length)],
-              color: colorArray[Math.floor(Math.random() * colorArray.length)],
-              disY: 1
-            };
-            balls.push(ball);
+      if(currentNums.length ==0){
+          currentNums = nums;
+      }else{
+          for(var index = 0;index<currentNums.length;index++){
+              if(currentNums[index].num!=nums[index].num){
+                  addBalls(nums[index]);
+                  currentNums[index].num=nums[index].num;
+              }
           }
-        }
       }
-    }
-
-    /*渲染*/
-    function render() {
-      //重置画布宽度，达到清空画布的效果
-      canvas.height = 100;
-      //渲染时钟
-      for (var i = 0; i < data.length; i++) {
-        renderDigit(i, data[i]);
-      }
-      //渲染小球
-      for (var i = 0; i < balls.length; i++) {
-        cxt.beginPath();
-        cxt.arc(balls[i].x, balls[i].y, R, 0, 2 * Math.PI);
-        cxt.fillStyle = balls[i].color;
-        cxt.closePath();
-        cxt.fill();
-      }
-    }
-
-    clearInterval(oTimer);
-    var oTimer = setInterval(function () {
-      //更新时钟
-      updateDigitTime();
-      //更新小球状态
+      renderBalls(cxt);
       updateBalls();
-      //渲染
-      render();
-    }, 50);
+
+      return date;
   }
+
+  function addBalls (item) {
+      var num = item.num;
+      var numMatrix = digit[num];
+      for(var y = 0;y<numMatrix.length;y++){
+          for(var x = 0;x<numMatrix[y].length;x++){
+              if(numMatrix[y][x]==1){
+                  var ball={
+                      offsetX:item.offsetX+RADIUS+RADIUS*2*x,
+                      offsetY:30+RADIUS+RADIUS*2*y,
+                      color:colors[Math.floor(Math.random()*colors.length)],
+                      g:1.5+Math.random(),
+                      vx:Math.pow(-1, Math.ceil(Math.random()*10))*4+Math.random(),
+                      vy:-5
+                  }
+                  balls.push(ball);
+              }
+          }
+      }
+  }
+
+  function renderBalls(cxt){
+      for(var index = 0;index<balls.length;index++){
+          cxt.beginPath();
+          cxt.fillStyle=balls[index].color;
+          cxt.arc(balls[index].offsetX, balls[index].offsetY, RADIUS, 0, 2*Math.PI);
+          cxt.fill();
+      }
+  }
+
+  function updateBalls () {
+      var i =0;
+      for(var index = 0;index<balls.length;index++){
+          var ball = balls[index];
+          ball.offsetX += ball.vx;
+          ball.offsetY += ball.vy;
+          ball.vy+=ball.g;
+          if(ball.offsetY > (WINDOW_HEIGHT-RADIUS)){
+              ball.offsetY= WINDOW_HEIGHT-RADIUS;
+              ball.vy=-ball.vy*u;
+          }
+          if(ball.offsetX>RADIUS&&ball.offsetX<(WINDOW_WIDTH-RADIUS)){
+
+              balls[i]=balls[index];
+              i++;
+          }
+      }
+      for(;i<balls.length;i++){
+          balls.pop();
+      }
+  }
+  function drawSingleNumber(offsetX, offsetY, num, cxt){
+      var numMatrix = digit[num];
+      for(var y = 0;y<numMatrix.length;y++){
+          for(var x = 0;x<numMatrix[y].length;x++){
+              if(numMatrix[y][x]==1){
+                  cxt.beginPath();
+                  cxt.arc(offsetX+RADIUS+RADIUS*2*x,offsetY+RADIUS+RADIUS*2*y,RADIUS,0,2*Math.PI);
+                  cxt.fill();
+              }
+          }
+      }
+      cxt.beginPath();
+      offsetX += numMatrix[0].length*RADIUS*2;
+      return offsetX;
+  }
+
+  var canvas = document.getElementById("canvasDiyBlock");
+  canvas.width=WINDOW_WIDTH;
+  canvas.height=WINDOW_HEIGHT;
+  context = canvas.getContext("2d");
+
+  var currentDate = new Date();
+
+  setInterval(function(){
+      context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+      drawDatetime(context);
+  }, 50)
 })();
