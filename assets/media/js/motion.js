5597b5540a28908afbf8a50eb5672d9045b1ae12
hsxyhao <806649821@qq.com>
2021-06-03 14:30:36 +0800

    优化

--- assets/media/js/motion.js
+++ assets/media/js/motion.js
@@ -17,7 +17,7 @@ var MotionExector = {
     this.next();
   }, 
   clear: function() {
-    window.Velocity('stop');
+    jQuery.Velocity('stop');
     this.queue = [];
     this.index - 1;
   }
@@ -62,7 +62,7 @@ let logoLineMotion = function(MotionExector) {
         }
       }
     })
-    window.Velocity.RunSequence(sequence);
+    jQuery.Velocity.RunSequence(sequence);
   } else{
     MotionExector.next();
   }
@@ -112,13 +112,13 @@ let titleMotion = function(MotionExector) {
       }
     })
   }
-  window.Velocity.RunSequence(sequence);
+  jQuery.Velocity.RunSequence(sequence);
 }
 
 let menuMotion = function(MotionExector) {
   let menus = document.querySelectorAll(Element_Class.navItem);
 
-  menus && window.Velocity(menus, 'transition.slideDownIn', {
+  menus && jQuery.Velocity(menus, 'transition.slideDownIn', {
     display: null,
     duration: 200,
     complete: function () {
@@ -133,15 +133,15 @@ let postListMotion = function(MotionExector) {
   || document.querySelector('.gemini .sidebar');
 
   if (geminiSiteMeta) {
-    window.Velocity(posts, 'transition.slideDownIn', { })
+    jQuery.Velocity(posts, 'transition.slideDownIn', { })
   } else {
-    window.Velocity(posts, 'transition.slideDownIn', { 
+    jQuery.Velocity(posts, 'transition.slideDownIn', { 
       complete: function() {
         MotionExector.next();
       }
     })
   }
-  geminiSiteMeta && window.Velocity(geminiSiteMeta,'transition.slideUpIn',{
+  geminiSiteMeta && jQuery.Velocity(geminiSiteMeta,'transition.slideUpIn',{
     stagger: 100, 
     drag: true,
     complete: function() {
@@ -158,7 +158,7 @@ let tagPostMotion = function(MotionExector) {
   let tagNodes = document.querySelectorAll(Element_Class.tagPostNode);
 
   if (tagNodes.length > 0) {
-    window.Velocity(tagNodes ,'transition.slideDownIn', {
+    jQuery.Velocity(tagNodes ,'transition.slideDownIn', {
       stagger: 100,
       drag: true,
       complete: function() {
@@ -204,7 +204,7 @@ window.addEventListener('load', function() {
     });
   }
   if (sequence.length > 0) {
-    window.Velocity.RunSequence(sequence);
+    jQuery.Velocity.RunSequence(sequence);
   }
   // 标签页入场动画
   let cloudTags = document.querySelectorAll(Element_Class.cloudTags)
@@ -214,6 +214,6 @@ window.addEventListener('load', function() {
       e: cloudTags,
       p: 'transition.expandIn'
     });
-    window.Velocity.RunSequence(sequence);
+    jQuery.Velocity.RunSequence(sequence);
   }
 })
